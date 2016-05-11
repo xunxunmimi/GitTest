67cc3c46c95b7a40410a3524a69f93db783acf82
cc <314044090@qq.com>
2016-05-11 19:44:31 +0800

    addFile

---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 cc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
