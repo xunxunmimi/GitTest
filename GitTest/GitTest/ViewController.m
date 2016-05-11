e1881b2870b671eb7f806d3c80d6e0c74d4ac9a9
lanou3g <476879926@qq.com>
2016-05-11 20:20:03 +0800

    修改了背景色

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
     self.view.backgroundColor = [UIColor cyanColor];
     
 }
